f83a3dbe831ef8eb7d258d7ecb1eb768cd7f6d67
sumithanvat <skhanvat@gmail.com>
2023-02-10 02:14:30 +0530

    add : UC8-sortingResultInDicitionaryArray added

--- sortingArithmeticComputationProblem.sh
+++ sortingArithmeticComputationProblem.sh
@@ -29,3 +29,26 @@ done
 
 echo "The computation results array is"
 echo ${array[@]}
+
+size=${#array[@]}
+for(( i = 0; i < size - 1; i++ ))
+do
+    swapped=0
+    for(( j = 0; j < size - i - 1; j++))
+    do
+        if((array[j] < array[j+1]))
+        then
+            temp=${array[j]}
+            array[j]=${array[j+1]}
+            array[j+1]=$temp
+            swapped=1
+        fi
+    done
+    if((swapped==0))
+    then
+        break
+    fi
+done
+
+echo "The Computation results sorted in descending order are: "
+echo ${array[@]}
